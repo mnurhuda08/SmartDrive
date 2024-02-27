d000b1e367cc55305c316901c9d360cfd07bf5e6
codeiqbal <>
2024-02-27 23:09:13 +0700

    restructuring base layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardLayoutComponent } from './components/layout/dashboard-layout/dashboard-layout.component';
+import { LoginLayoutComponent } from './components/layout/login-layout/login-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '',
+        pathMatch: 'full',
+      },
+      {
+        path: '',
+        loadChildren: () =>
+          import('./modules/dashboard/dashboard.module').then(
+            (m) => m.DashboardModule
+          ),
+      },
+    ],
+  },
+  // login
+  {
+    path: '',
+    component: LoginLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('./modules/login/login.module').then((m) => m.LoginModule),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/layout/sidebar/sidebar.component.ts
+++ src/app/components/layout/sidebar/sidebar.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { MenuConstant } from 'src/app/constants/menu-constant';
+import { IMenus } from 'src/app/interfaces/common/i-menus';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
+  menus: any[] = [];
+  filteredMenus: IMenus[] = [];
+  roles: string[] = [];
 
+  constructor() {
+    this.menus = MenuConstant;
+    const myRoles = ['AD', 'CU', 'EM', 'PC', 'PR'];
+
+    MenuConstant.filter((menuItem) =>
+      menuItem.roles.some((role) => myRoles.includes(role))
+    ).map((item) => this.filteredMenus.push(item));
+  }
 }
