3d5551f619e8d4a8a87f6feea71368b9561d69ab
Agus <agusprastia1@gmail.com>
2024-03-05 16:55:10 +0700

    fix payment-transaction paging

--- src/app/components/payment/payment-transaction/payment-transaction.component.ts
+++ src/app/components/payment/payment-transaction/payment-transaction.component.ts
@@ -13,33 +13,47 @@ export class PaymentTransactionComponent implements OnInit {
   title = "paging";
   page: number = 1;
   count: number = 0;
-  tableSize: number = 5;
+  tableSize: number =3;
   tableSizes: any = [5, 10, 15, 25]
-
+  userId: number = 2
   @ViewChild(MatSort) sort!: MatSort
-  constructor(private paymentService: PaymentTransactionService) {
+  constructor(private paymentTrxService: PaymentTransactionService) {
     this.data = []
-
+ 
   }
   ngOnInit(): void {
-    this.getAllPaymentTransaction();
+    // this.getAllPaymentTransaction();
+    this.paymentTrxService.getPaymentTransactionsCount(this.userId).subscribe(count => {
+      console.log("Received count from API:", count);
+      this.count = count;
+      console.log("Updated count:", this.count);
+    });
+    this.getAllPaymentTransactionByPage()
   }
 
-  getAllPaymentTransaction() {
-    this.paymentService
-      .getPaymentTransactionsByUserId(2)
-      .subscribe((result: PaymentTransaction[]) => (this.data = result))
+  // getAllPaymentTransactionByPage() {
+  //   this.paymentService.getPaymentTransactionsPaging(2, this.page, this.tableSize)
+  //     .subscribe((result: PaymentTransaction[]) => (this.data = result))
+  // }
+ 
+  getAllPaymentTransactionByPage() {
+    this.paymentTrxService.getPaymentTransactionsPaging(this.userId, this.page, this.tableSize)
+      .subscribe((result: { data: PaymentTransaction[], count: number }) => {
+        this.data = result; 
+      });
   }
 
   onTableDataChange(event: any) {
     this.page = event
-    this.getAllPaymentTransaction();
+    this.getAllPaymentTransactionByPage()
+    // this.getAllPaymentTransaction();
   }
 
   onTableSizeChange(event: any): void {
     this.tableSize = event.target.value;
     this.page = 1;
-    this.getAllPaymentTransaction();
+    this.getAllPaymentTransactionByPage()
+
   }
 
 }
--- src/app/services/payment/payment-transaction.service.ts
+++ src/app/services/payment/payment-transaction.service.ts
@@ -1,25 +1,45 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { PaymentTransaction } from 'src/app/models/payment/PaymentTransaction';
 import { environment } from 'src/environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaymentTransactionService {
-  url = "PaymentTransactions"
+  url = 'PaymentTransactions';
   constructor(private http: HttpClient) { }
 
   public getPaymentTransactions(): Observable<PaymentTransaction[]> {
-    return this.http.get<PaymentTransaction[]>(`${environment.baseUrl}/${this.url}`);
+    return this.http.get<PaymentTransaction[]>(
+      `${environment.baseUrl}/${this.url}`
+    );
   }
 
-  public getPaymentTransactionsByUserId(userId: number): Observable<PaymentTransaction[]> {
-    return this.http.get<PaymentTransaction[]>(`${environment.baseUrl}/${this.url}/${userId}`);
+  public getPaymentTransactionsCount(userId: number): Observable<number> {
+    const countUrl = `${environment.baseUrl}/${this.url}/pagingCount?UserEntityId=${userId}`;
+    return this.http.get<number>(countUrl);
   }
 
-  public createPaymentTransaction(PaymentTransaction: PaymentTransaction): Observable<PaymentTransaction[]> {
-    return this.http.post<PaymentTransaction[]>(`${environment.baseUrl}/${this.url}`, PaymentTransaction);
+  public getPaymentTransactionsPaging(
+    userId: number,
+    pageNumber: null | number = null,
+    pageSize: null | number = null,
+    accountNumber: null | string = null,
+    accountBussEntity: null | number = null
+  ): Observable<{ data: PaymentTransaction[]; count: number }> {
+    const url = `${environment.baseUrl}/${this.url}/paging?UserEntityId=${userId}&PageNumber=${pageNumber}&PageSize=${pageSize}`;
+    const count = `${environment.baseUrl}/${this.url}/paging?UserEntityId=${userId}`
+    return this.http.get<{ data: PaymentTransaction[]; count: number }>(url);
+  }
+
+  public createPaymentTransaction(
+    PaymentTransaction: PaymentTransaction
+  ): Observable<PaymentTransaction[]> {
+    return this.http.post<PaymentTransaction[]>(
+      `${environment.baseUrl}/${this.url}`,
+      PaymentTransaction
+    );
   }
 }
